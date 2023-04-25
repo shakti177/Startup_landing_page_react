7bd3bfdd5fefbe25ef88f48d2d0188cb55b611e9
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2023-04-25 15:20:02 +0530

    Initial Commits

--- src/Components/LandingPage/Solutions/Solution.jsx
+++ src/Components/LandingPage/Solutions/Solution.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Solution.css'
 
 const Solution = () => {
   return (
-    <div>Solution</div>
+    <section id="solution_section">
+      <div className="solution_container">
+        <div className="solution_headings">
+          <button>Reach goals that matter</button>
+          <h2>One product, unlimited solution</h2>
+          <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
