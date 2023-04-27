fe8c3907b597809b201235b767e9e2156f4638ae
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2023-04-27 12:03:42 +0530

    Initial Commits

--- src/Components/ForgetPassword/Forget.jsx
+++ src/Components/ForgetPassword/Forget.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './Forget.css'
+import React from "react";
+import "./Forget.css";
+import { Link } from "react-router-dom";
 
 const Forget = () => {
   return (
-    <div>Forget</div>
-  )
-}
+    <section className="forget_main">
+      <div className="forget_section">
+        <div className="forget_content">
+          <h1>Forgot your password?</h1>
+          <p>We'll email you instructions on how to reset it.</p>
+        </div>
+        <div className="forget_form_section">
+          <form className="forget_form_feilds">
+            <div className="forget_feild">
+              <label htmlFor="email">Email</label>
+              <input type="text" placeholder="you@yourcompany.com" required />
+            </div>
 
-export default Forget
+            <div className="forget_button">
+              <button type="submit">Reset Password</button>
+            </div>
+          </form>
+        </div>
+        <div className="forget_last_line">
+          <Link to="/signin">
+            <span>Cancel</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Forget;
